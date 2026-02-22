7a4e3d05ca6cff596d6ad48d5dce87f65af5ea90
DAyansh <dayansh1503@gmail.com>
2026-02-22 09:57:17 +0530

    Changes

--- src/main/java/com/ayansh/Backend/Service/CloudinaryFileStorageService.java
+++ src/main/java/com/ayansh/Backend/Service/CloudinaryFileStorageService.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.*;
 
 @Service
-@Profile("cloudinary")
+@Profile("prod")
 public class CloudinaryFileStorageService implements FileStorageService {
 
     @Value("${cloudinary.cloud_name:}")
